00ae006b7284e8752cd7cd4982da8433b71ee9c4
Prince Cheruvathur <prncher@users.noreply.github.com>
2017-02-02 09:31:16 -0800

    Add typedefs for keyvault

---
+++ azure.keyvault.typings/index.d.ts
@@ -0,0 +1,22 @@
+﻿declare module 'azure-keyvault' {
+    var azureKeyVault: azureKeyVault;
+
+    export = azureKeyVault;
+}
+
+interface azureKeyVault {
+    KeyVaultCredentials: KeyVaultCredentials;
+    KeyVaultClient: KeyVaultClient;
+}
+
+interface KeyVaultCredentials {
+    (authenticator: Function): void;
+}
+
+interface KeyVaultClient {
+    (credentials: KeyVaultCredentials, options?: any): void;
+    getKeys(vaultBaseUrl: string, options: any, callback: Function);
+    getSecrets(vaultBaseUrl: string, options: any, callback: Function);
+    getSecret(secretIdentifier: string, options: any, callback: Function);
+    getSecret(secretIdentifier: string, callback: Function);
+}
